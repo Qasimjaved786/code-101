84785fe53dc588602fe56c588c4e4dda16dc3113
Qasimjaved786 <56893904+Qasimjaved786@users.noreply.github.com>
2021-01-20 20:05:10 +0000

    Update Loops.js

--- JavaScript/Loops.js
+++ JavaScript/Loops.js
@@ -2,12 +2,5 @@ let friendsAtYourParty =24;
 while(friendsAtYourParty<24){
     friendsAtYourParty++;
 }
+//we use "while" instead of "Let" coz it is reassigned{
 console.log(friendsAtYourParty);
-
-
-
-
-
-
-
-    
