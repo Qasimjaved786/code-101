e9300eee804ab468bb160c64166144680c0e3d6b
Qasimjaved786 <56893904+Qasimjaved786@users.noreply.github.com>
2021-01-20 13:29:09 +0000

    string concatenation

---
+++ JavaScript/string concatenation.js
@@ -0,0 +1,8 @@
+const firstName = "Qasim";
+const lastName = "Javed";
+
+const sentence = "Hello " + firstName + " " + lastName + "! How are you!?";
+const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
+
+console.log(sentence);
+console.log(sentenceWithTemplate);
