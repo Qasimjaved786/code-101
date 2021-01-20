4537370fa5af07c40c8b1f1a669bcd8f79bda598
Qasimjaved786 <56893904+Qasimjaved786@users.noreply.github.com>
2021-01-20 22:45:18 +0000

    Create Functions JS.js

---
+++ JavaScript/Functions JS.js
@@ -0,0 +1,6 @@
+function greet(firstName, lastName, honorofic, greeting) {
+    return `${greeting} ${honorofic} ${lastName}! I am PLEASED that we met, ${firstName}!,I hope you enjoyed here,${honorofic} ${lastName}.`;
+}
+console.log(greet("Qasim", "Javed", "Janeb", "Welcome"));
+
+
