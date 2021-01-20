1b129e61b259d1af2ae62dc42c02775dab5c85b4
Qasimjaved786 <56893904+Qasimjaved786@users.noreply.github.com>
2021-01-20 19:39:57 +0000

    Update Loops.js

--- JavaScript/Loops.js
+++ JavaScript/Loops.js
@@ -1,5 +1,5 @@
 let friendsAtYourParty = 0;
-//we use let coz it is reassigned{
+//we use "let" instead of "const" coz it is reassigned{
 while (friendsAtYourParty < 10) {
   friendsAtYourParty = friendsAtYourParty + 1;
 }
