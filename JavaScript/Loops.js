14b228fb8daf980c22ba465cbb8b6e3a5ed5eb7e
Qasimjaved786 <56893904+Qasimjaved786@users.noreply.github.com>
2021-01-20 19:38:46 +0000

    Create Loops.js

---
+++ JavaScript/Loops.js
@@ -0,0 +1,6 @@
+let friendsAtYourParty = 0;
+//we use let coz it is reassigned{
+while (friendsAtYourParty < 10) {
+  friendsAtYourParty = friendsAtYourParty + 1;
+}
+console.log(friendsAtYourParty);
