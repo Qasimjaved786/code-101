ccd9d6aa0f7cdb2325cd85e5629fd5e742e6d460
Qasimjaved786 <56893904+Qasimjaved786@users.noreply.github.com>
2021-01-22 22:59:42 +0000

    Create Builtins JS.js

---
+++ JavaScript/Builtins JS.js
@@ -0,0 +1,7 @@
+const sentence="ThIs HaS wEiRd CaSiNg On It";
+console.log(sentence.toLowerCase());
+//"Builtins" is actually the representation of the letters,either in the form of uppercase or in lowercase
+
+    const sentence = "ThIs HaS wEiRd CaSiNg On It";
+console.log(sentence.toLowerCase());
+//"this" is  what it is
