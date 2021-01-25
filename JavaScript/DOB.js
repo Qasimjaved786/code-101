9863722bcff4d85697d52963e228259446da787d
Qasimjaved786 <56893904+Qasimjaved786@users.noreply.github.com>
2021-01-25 23:14:01 +0000

    DOB.Js

---
+++ JavaScript/DOB.js
@@ -0,0 +1,14 @@
+<style>
+  .red-square {
+    width: 100px;
+    height: 100px;
+    background-color: crimson;
+  }
+</style>
+
+<div class="red-square"></div>
+
+<script>
+  const redSquare = document.querySelector('.red-square');
+  redSquare.style.backgroundColor = 'limegreen';
+</script>
