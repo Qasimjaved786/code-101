efa00ceda7fb5fde2dc3e423e211fa016c97d89a
Qasimjaved786 <56893904+Qasimjaved786@users.noreply.github.com>
2021-01-26 22:49:35 +0000

    Create Effects & Listeners.js

---
+++ JavaScript/Effects & Listeners.js
@@ -0,0 +1,10 @@
+<input placeholder="type into me!" class="input-to-copy" />
+<p class="p-to-copy-to">Nothing has happened yet.</p>
+<script>
+  const input = document.querySelector('.input-to-copy');
+  const paragraph = document.querySelector('.p-to-copy-to');
+
+  input.addEventListener("keyup", function() {
+    paragraph.innerText  = input.value;
+  });
+</script>
